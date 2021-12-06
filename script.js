a53432c7a186a5009865e6c63b0cb0e649489d85
a-Very-Horny-Oni <theH0rny0n1@protonmail.com>
2021-12-05 23:09:15 -0600

    fourth commit

--- script.js
+++ script.js
@@ -53,39 +53,57 @@ console.log(money);
 //array to push the bills of the given amount of money
 var delivered;
 
+
 function give_money(){
     delivered = [];
     money = read_money();
     console.log(money);
     
-    for(var bill of cash){
-        if(money > 0){
-            div = Math.floor(money / bill.value);
-            if(div > bill.amount){
-                b_amount = bill.amount;
-            }
-            else{
-                b_amount = div;
-            }
-
-            delivered.push(new Bills(bill.value, b_amount));
-            money -= (bill.value * b_amount);
-        }
-    }
     if(money > 0){
-        result.innerHTML = "I cannot deliver your requested amount, not enough bills";
-    }
-    else{
-        result.innerHTML += "Movement #" + count_movs + "<br />";
-        for( var del of delivered){
-            if(del.amount > 0){
-                result.innerHTML += del.amount + " bills of $" + del.value + "<br />";
-            }
-        }
-        count_movs += 1;
-        result.innerHTML += "<br /><br />";
+        var count_bill = 0;
+        for(var bill of cash){
+            if(money > 0){
+                div = Math.floor(money / bill.value);
+                if(div > bill.amount){
+                    b_amount = bill.amount;
+                }
+                else{
+                    b_amount = div;
+                }
+    
+                delivered.push(new Bills(bill.value, b_amount));
+                money -= (bill.value * b_amount);
 
-        console.log(delivered);
-        //show the delivered bills and amount
+                //console.log("iteration: " + count_bill);
+                
+                //discount amount from bills and show them in console
+                console.log(bill);
+                cash[count_bill] = new Bills (bill.value, bill.amount - b_amount);
+                
+                console.log("Bills left after deliver.");
+                console.log(cash);
+                
+                //re-count the bills
+                check_money();
+            }
+            count_bill += 1;
+        }
+        if(money > 0){
+            result.innerHTML = "I cannot deliver your requested amount, not enough bills <br /><br />";
+        }
+        else{
+            result.innerHTML += "Movement #" + count_movs + "<br />";
+            for( var del of delivered){
+                if(del.amount > 0){
+                    result.innerHTML += del.amount + " bills of $" + del.value + "<br />";
+                }
+            }
+            count_movs += 1;
+            result.innerHTML += "<br /><br />";
+
+            //show the delivered bills and amount
+            console.log("Delivered money");
+            console.log(delivered);
+        }
     }
 }
